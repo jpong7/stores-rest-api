298bf17b94be0ab6d9efc0961894f96cbff5379c
Bovornpot Jindachuen <bovornpot.ji@glsict.com>
2022-03-05 07:47:38 +0700

    Fix os environ

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from resources.store import Store, StoreList
 #from datetime import timedelta
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgresql://lvmugkjuslrpax:95b6e793cea469d9a0c0aea43d15978842853475b2b657ad2dea12a69a7fdbc4@ec2-44-194-113-156.compute-1.amazonaws.com:5432/d8c868pqe4op0n', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800) # config JWT to expire within half an hour
 app.secret_key = 'jose'
